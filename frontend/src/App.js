ecd5d1a42b6ba58950ab46760fe6db4b7739a853
Chaitanya Gosavi <chaitanyagosavi540@gmail.com>
2023-02-03 01:36:47 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./global.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navigation from "./components/shared/Navigation/Navigation";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
